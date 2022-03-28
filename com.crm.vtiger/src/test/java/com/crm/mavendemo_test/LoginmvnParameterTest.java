5fdec64563d80d3ffb3b4ad7a9bf9e2da9a1193f
Sumit Pathak <Sumit Pathak@LAPTOP-S5GSA3HC>
2022-03-28 14:08:37 +0530

    push

--- com.crm.vtiger/src/test/java/com/crm/mavendemo_test/LoginmvnParameterTest.java
+++ com.crm.vtiger/src/test/java/com/crm/mavendemo_test/LoginmvnParameterTest.java
@@ -15,38 +15,41 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class LoginmvnParameterTest {
 
 	@Test
-	public void login() {
-		WebDriver driver=null;
-		String url=System.getProperty("url");
-		String username=System.getProperty("username");
-		String password=System.getProperty("password");
-		String browser=System.getProperty("browser");
+	public void a() {
 		
 		
-		
-		if(browser.contains("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			driver=new ChromeDriver();
-		}
-		else if (browser.contains("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver=new FirefoxDriver();
-		}
-		else {
-			System.out.println("use valid browser");
-		}
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-		driver.get(url);
-		
-		driver.findElement(By.name("user_name")).sendKeys(username);
-		driver.findElement(By.name("user_password")).sendKeys(password);
-		driver.findElement(By.id("submitButton")).click();
-		
-		WebElement lookup = driver.findElement(By.xpath("//img[@src=\"themes/softed/images/user.PNG\"]"));
-		Actions a=new Actions(driver);
-		a.moveToElement(lookup).perform();
-		driver.findElement(By.linkText("Sign Out")).click();
-		driver.close();
+//	public void login() {
+//		WebDriver driver=null;
+//		String url=System.getProperty("url");
+//		String username=System.getProperty("username");
+//		String password=System.getProperty("password");
+//		String browser=System.getProperty("browser");
+//		
+//		
+//		
+//		if(browser.contains("chrome")) {
+//			WebDriverManager.chromedriver().setup();
+//			driver=new ChromeDriver();
+//		}
+//		else if (browser.contains("firefox")) {
+//			WebDriverManager.firefoxdriver().setup();
+//			driver=new FirefoxDriver();
+//		}
+//		else {
+//			System.out.println("use valid browser");
+//		}
+//		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+//		driver.get(url);
+//		
+//		driver.findElement(By.name("user_name")).sendKeys(username);
+//		driver.findElement(By.name("user_password")).sendKeys(password);
+//		driver.findElement(By.id("submitButton")).click();
+//		
+//		WebElement lookup = driver.findElement(By.xpath("//img[@src=\"themes/softed/images/user.PNG\"]"));
+//		Actions a=new Actions(driver);
+//		a.moveToElement(lookup).perform();
+//		driver.findElement(By.linkText("Sign Out")).click();
+//		driver.close();
 	}
 }
