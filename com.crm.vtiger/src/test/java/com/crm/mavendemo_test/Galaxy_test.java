58ce31a8f9c2114ca66d5d5783218d38db228081
Sumit Pathak <Sumit Pathak@LAPTOP-S5GSA3HC>
2022-03-28 14:21:06 +0530

    galaxy push

---
+++ com.crm.vtiger/src/test/java/com/crm/mavendemo_test/Galaxy_test.java
@@ -0,0 +1,15 @@
+package com.crm.mavendemo_test;
+
+import org.testng.annotations.Test;
+
+public class Galaxy_test {
+	@Test
+	public void univrese() {
+		System.out.println("infinite");
+	}
+	@Test
+	public void milkyway() {
+		System.out.println("beautiful");
+	}
+
+}
